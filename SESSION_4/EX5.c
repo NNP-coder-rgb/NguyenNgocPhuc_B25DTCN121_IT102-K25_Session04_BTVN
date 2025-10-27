ce59cb8f925d232152bb15ef01c9d44d93486a12
NNP-coder-rgb <nguyenphuc230607@gmail.com>
2025-10-27 22:11:22 +0700

    Update EX5.c

--- SESSION_4/EX5.c
+++ SESSION_4/EX5.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
 
-int main(){
-	float chi_so_cu, chi_so_moi;
-	printf("Nhap chi so dien cu: ");
-	scanf("%f",&chi_so_cu);
-	
-	printf("Nhap chi so dien moi: ");
-	scanf("%f",&chi_so_moi);
-	
-	float so_dien, so_tien;
-	so_dien = chi_so_moi - chi_so_cu;
-	if(so_dien < 0){
-		printf("Loi: chi so moi phai lon hon chi so cu\n");
-	}else
-		printf("So dien ho gia dinh su dung trong thang la: %.0fKwh\n",so_dien);
-	if(so_dien>= 0 && so_dien < 50){
-		so_tien = so_dien * 10000;
-		printf("So tien dien thang do gia dinh phai tra la: %.0f VND",so_tien);
-	}else if(so_dien >= 50 && so_dien < 100){
-		so_tien = 50*10000 + (so_dien - 50) * 15000;
-		printf("So tien dien thang do gia dinh phai tra la: %.0f VND",so_tien);
-	}else if(so_dien >=100 && so_dien <150){
-		so_tien = 50*10000 + 50*15000 + (so_dien - 100) * 20000;
-		printf("So tien dien thang do gia dinh phai tra la: %.0f VND",so_tien);
-	}else if(so_dien >= 150 && so_dien <200){
-		so_tien = 50*10000 + 50*15000 + 50*20000 + (so_dien - 150)*25000;
-		printf("So tien dien thang do gia dinh phai tra la: %.0f VND",so_tien);
-	}else if(so_dien>=200){
-		so_tien = 50*10000 + 50*15000 + 50*20000 + 50*25000 + (so_dien - 200)*30000;
-		printf("So tien dien thang do gia dinh phai tra la: %.0f VND",so_tien);
-	}
-	return 0; 
+int main (){
+    int num1,num2,num3;
+    printf("Nhap vao so thu nhat:");
+    scanf("%d",&num1);
+
+    printf("Nhap vao so thu hai:");
+    scanf("%d",&num2);
+
+    printf("Nhap vao so thu ba:");
+    scanf("%d",&num3);
+    
+    if(num3 > num1 && num3 < num2){
+        printf("So %d nam trong khoang giua so %d va %d ",num3,num1,num2);
+    }else{
+        printf("So %d khong nam trong khoang giua so %d va %d",num3,num1,num2);
+    }
+    return 0;
 }
