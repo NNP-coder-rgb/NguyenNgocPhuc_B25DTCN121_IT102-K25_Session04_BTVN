e6ce8b31a3b63a51919f963c8c7a8fb1b29f158b
NNP-coder-rgb <nguyenphuc230607@gmail.com>
2025-10-27 22:37:51 +0700

    Enhance triangle check with positive value validation
    Added checks for positive integer values for triangle sides.

--- SESSION_4/EX8.c
+++ SESSION_4/EX8.c
@@ -11,10 +11,11 @@ int main()
 	
 	printf("Nhap so nguyen c:");
 	scanf("%d",&c);
-	if(a+b>c && a+c>b && b+c>a){
+	if(a+b>c && a+c>b && b+c>a && a > 0 && b > 0 && c > 0){
 		printf("La 3 canh cua tam giac");
 	}else{
 		printf("Khong phai 3 canh cua tam giac");
 	}
 	return 0;
 }
+
